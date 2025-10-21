9242c53c2f044d7b30aaa83818a7a5880d640017
José Manuel Mora Zúñiga <jose.morazuniga@ucr.ac.cr>
2025-10-20 18:10:46 -0600

    Added a new weapon that reduces armor on hit

---
+++ src/models/battle_axe.py
@@ -0,0 +1,18 @@
+from src.models.weapon import Weapon
+
+class BattleAxe(Weapon):
+    def attack(self, attacker, target):
+        """
+        Return the damage dealt by the weapon
+
+        Any other effects such as buffs or debuffs
+        should be handled before returning.
+        """
+
+        # The battle axe reduces the target's armor by 5 points on each hit
+        # Note: Armor could be negative to give extra damage,
+        # each defense system should check if armor is outside its  own boundaries
+        target.armor -= 5
+
+        # Battle axe has a fixed damage of 20
+        return 20
